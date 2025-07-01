909aeed2e7302e62825ff4d31331f30d65216b0c
AhmedHanafy725 <hanafya@incubaid.com>
2025-07-01 15:20:17 +0300

    Adjust HA cluster with dual stack cluster

--- k3s/scripts/entrypoint.sh
+++ k3s/scripts/entrypoint.sh
@@ -10,6 +10,11 @@ if [ -z "${K3S_FLANNEL_IFACE}" ]; then
     K3S_FLANNEL_IFACE=eth0
 fi
 
+if [[ "${DUAL_STACK}" = "true" && "${MASTER}" = "true" ]]; then
+    EXTRA_ARGS="$EXTRA_ARGS --cluster-cidr=10.42.0.0/16,2001:cafe:42::/56"
+    EXTRA_ARGS="$EXTRA_ARGS --service-cidr=10.43.0.0/16,2001:cafe:43::/112"
+fi
+
 if [ -z "${K3S_URL}" ]; then
     # Add additional SANs for planetary network IP, public IPv4, and public IPv6  
     # https://github.com/threefoldtech/tf-images/issues/98
@@ -24,10 +29,6 @@ if [ -z "${K3S_URL}" ]; then
             ip route get $addr && EXTRA_ARGS="$EXTRA_ARGS --tls-san $addr"
         done
     done
-    if [ "${DUAL_STACK}" = "true" ]; then
-        EXTRA_ARGS="$EXTRA_ARGS --cluster-cidr=10.42.0.0/16,2001:cafe:42::/56"
-        EXTRA_ARGS="$EXTRA_ARGS --service-cidr=10.43.0.0/16,2001:cafe:43::/112"
-    fi
     if [ "${HA}" = "true" ]; then
         EXTRA_ARGS="$EXTRA_ARGS --cluster-init"
     fi
