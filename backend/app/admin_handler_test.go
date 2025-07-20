f5f9e87fd28c502cfcd62785e5b796c5eb820707
Alaa Elattar <alaamahmoud.1223@gmail.com>
2025-07-20 12:27:56 +0300

    refactor tests && rebase with user tests

--- backend/app/admin_handler_test.go
+++ backend/app/admin_handler_test.go
@@ -14,58 +14,37 @@ import (
 	"kubecloud/models"
 )
 
-// Helper to get admin token
-func getAdminToken(t *testing.T, app *App, id int, email, username string) string {
-	return GetAuthToken(t, app, id, email, username, true)
-}
-
 func TestListUsersHandler(t *testing.T) {
 	app, err := SetUp(t)
 	require.NoError(t, err)
 	router := app.router
 
-	// Register users
-	adminUser := &models.User{
-		ID:       1,
-		Username: "Admin User",
-		Email:    "admin@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    true,
-	}
-	normalUser := &models.User{
-		ID:       2,
-		Username: "Normal User",
-		Email:    "user@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    false,
-	}
-	err = app.handlers.db.RegisterUser(adminUser)
-	require.NoError(t, err)
-	err = app.handlers.db.RegisterUser(normalUser)
-	require.NoError(t, err)
+	adminUser := CreateTestUser(t, app, "admin@example.com", "Admin User", []byte("securepassword"), true, true, 0, time.Now())
+	normalUser := CreateTestUser(t, app, "user@example.com", "Normal User", []byte("securepassword"), true, false, 0, time.Now())
 
 	t.Run("Test List all users successfully", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
 		req, _ := http.NewRequest("GET", "/api/v1/users", nil)
 		req.Header.Set("Authorization", "Bearer "+token)
 		resp := httptest.NewRecorder()
 		router.ServeHTTP(resp, req)
 		assert.Equal(t, http.StatusOK, resp.Code)
-
-		type UsersResponse struct {
-			Message string `json:"message"`
-			Data    struct {
-				Users []models.User `json:"users"`
-			} `json:"data"`
-		}
-
-		var usersResp UsersResponse
+		var usersResp map[string]interface{}
 		err := json.Unmarshal(resp.Body.Bytes(), &usersResp)
 		assert.NoError(t, err)
-		assert.Equal(t, "Users are retrieved successfully", usersResp.Message)
-		users := usersResp.Data.Users
+		assert.Equal(t, "Users are retrieved successfully", usersResp["message"])
+
+		data, ok := usersResp["data"].(map[string]interface{})
+		assert.True(t, ok)
+		usersRaw, ok := data["users"]
+		assert.True(t, ok)
+		usersBytes, err := json.Marshal(usersRaw)
+		assert.NoError(t, err)
+
+		var users []models.User
+		err = json.Unmarshal(usersBytes, &users)
+		assert.NoError(t, err)
+
 		var foundAdmin, foundUser bool
 		for _, user := range users {
 			if user.Email == adminUser.Email {
@@ -101,39 +80,11 @@ func TestDeleteUsersHandler(t *testing.T) {
 	require.NoError(t, err)
 	router := app.router
 
-	adminUser := &models.User{
-		ID:       1,
-		Username: "Admin User",
-		Email:    "admin@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    true,
-	}
-	userToDelete := &models.User{
-		ID:       2,
-		Username: "Delete Me",
-		Email:    "deleteme@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    false,
-	}
-	nonAdminUser := &models.User{
-		ID:       3,
-		Username: "Normal User",
-		Email:    "user@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    false,
-	}
-	err = app.handlers.db.RegisterUser(adminUser)
-	require.NoError(t, err)
-	err = app.handlers.db.RegisterUser(userToDelete)
-	require.NoError(t, err)
-	err = app.handlers.db.RegisterUser(nonAdminUser)
-	require.NoError(t, err)
+	adminUser := CreateTestUser(t, app, "admin@example.com", "Admin User", []byte("securepassword"), true, true, 0, time.Now())
+	nonAdminUser := CreateTestUser(t, app, "user@example.com", "Normal User", []byte("securepassword"), true, false, 0, time.Now())
 
 	t.Run("Test Delete user successfully", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, adminUser.Admin)
 		req, _ := http.NewRequest("DELETE", "/api/v1/users/2", nil)
 		req.Header.Set("Authorization", "Bearer "+token)
 		resp := httptest.NewRecorder()
@@ -146,7 +97,7 @@ func TestDeleteUsersHandler(t *testing.T) {
 	})
 
 	t.Run("Test Admin deletes its account", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, adminUser.Admin)
 		req, _ := http.NewRequest("DELETE", "/api/v1/users/1", nil)
 		req.Header.Set("Authorization", "Bearer "+token)
 		resp := httptest.NewRecorder()
@@ -155,7 +106,7 @@ func TestDeleteUsersHandler(t *testing.T) {
 	})
 
 	t.Run("Test Delete with invalid user id", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, adminUser.Admin)
 		req, _ := http.NewRequest("DELETE", "/api/v1/users/aaa", nil)
 		req.Header.Set("Authorization", "Bearer "+token)
 		resp := httptest.NewRecorder()
@@ -164,7 +115,7 @@ func TestDeleteUsersHandler(t *testing.T) {
 	})
 
 	t.Run("Test Delete with no user id", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, adminUser.Admin)
 		req, _ := http.NewRequest("DELETE", "/api/v1/users/", nil)
 		req.Header.Set("Authorization", "Bearer "+token)
 		resp := httptest.NewRecorder()
@@ -173,7 +124,7 @@ func TestDeleteUsersHandler(t *testing.T) {
 	})
 
 	t.Run("Test Delete non-existing user", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, adminUser.Admin)
 		req, _ := http.NewRequest("DELETE", "/api/v1/users/9999", nil)
 		req.Header.Set("Authorization", "Bearer "+token)
 		resp := httptest.NewRecorder()
@@ -203,33 +154,15 @@ func TestGenerateVouchersHandler(t *testing.T) {
 	require.NoError(t, err)
 	router := app.router
 
-	adminUser := &models.User{
-		ID:       1,
-		Username: "Admin User",
-		Email:    "admin@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    true,
-	}
-	nonAdminUser := &models.User{
-		ID:       2,
-		Username: "Normal User",
-		Email:    "user@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    false,
-	}
-	err = app.handlers.db.RegisterUser(adminUser)
-	require.NoError(t, err)
-	err = app.handlers.db.RegisterUser(nonAdminUser)
-	require.NoError(t, err)
+	adminUser := CreateTestUser(t, app, "admin@example.com", "Admin User", []byte("securepassword"), true, true, 0, time.Now())
+	nonAdminUser := CreateTestUser(t, app, "user@example.com", "Normal User", []byte("securepassword"), true, false, 0, time.Now())
 
 	t.Run("Test GenerateVouchers successfully", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
-		payload := map[string]interface{}{
-			"count":             2,
-			"value":             10.0,
-			"expire_after_days": 7,
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
+		payload := GenerateVouchersInput{
+			Count:       2,
+			Value:       10.0,
+			ExpireAfter: 7,
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/vouchers/generate", bytes.NewReader(body))
@@ -245,14 +178,19 @@ func TestGenerateVouchersHandler(t *testing.T) {
 		assert.NotNil(t, result["data"])
 		data, ok := result["data"].(map[string]interface{})
 		assert.True(t, ok)
-		vouchers, ok := data["vouchers"].([]interface{})
+		vouchersRaw, ok := data["vouchers"]
 		assert.True(t, ok)
+		vouchersBytes, err := json.Marshal(vouchersRaw)
+		assert.NoError(t, err)
+		var vouchers []models.Voucher
+		err = json.Unmarshal(vouchersBytes, &vouchers)
+		assert.NoError(t, err)
 		assert.Len(t, vouchers, 2)
 	})
 
 	t.Run("Test GenerateVouchers with invalid request format", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
-		body, _ := json.Marshal(map[string]interface{}{}) // missing required fields
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
+		body, _ := json.Marshal(map[string]interface{}{})
 		req, _ := http.NewRequest("POST", "/api/v1/vouchers/generate", bytes.NewReader(body))
 		req.Header.Set("Authorization", "Bearer "+token)
 		req.Header.Set("Content-Type", "application/json")
@@ -262,10 +200,10 @@ func TestGenerateVouchersHandler(t *testing.T) {
 	})
 
 	t.Run("Test GenerateVouchers with no token", func(t *testing.T) {
-		payload := map[string]interface{}{
-			"count":             1,
-			"value":             5.0,
-			"expire_after_days": 3,
+		payload := GenerateVouchersInput{
+			Count:       1,
+			Value:       5.0,
+			ExpireAfter: 3,
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/vouchers/generate", bytes.NewReader(body))
@@ -277,10 +215,10 @@ func TestGenerateVouchersHandler(t *testing.T) {
 
 	t.Run("Test GenerateVouchers with non-admin user", func(t *testing.T) {
 		token := GetAuthToken(t, app, nonAdminUser.ID, nonAdminUser.Email, nonAdminUser.Username, false)
-		payload := map[string]interface{}{
-			"count":             1,
-			"value":             5.0,
-			"expire_after_days": 3,
+		payload := GenerateVouchersInput{
+			Count:       1,
+			Value:       5.0,
+			ExpireAfter: 3,
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/vouchers/generate", bytes.NewReader(body))
@@ -297,26 +235,8 @@ func TestListVouchersHandler(t *testing.T) {
 	require.NoError(t, err)
 	router := app.router
 
-	adminUser := &models.User{
-		ID:       1,
-		Username: "Admin User",
-		Email:    "admin@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    true,
-	}
-	nonAdminUser := &models.User{
-		ID:       2,
-		Username: "Normal User",
-		Email:    "user@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    false,
-	}
-	err = app.handlers.db.RegisterUser(adminUser)
-	require.NoError(t, err)
-	err = app.handlers.db.RegisterUser(nonAdminUser)
-	require.NoError(t, err)
+	adminUser := CreateTestUser(t, app, "admin@example.com", "Admin User", []byte("securepassword"), true, true, 0, time.Now())
+	nonAdminUser := CreateTestUser(t, app, "user@example.com", "Normal User", []byte("securepassword"), true, false, 0, time.Now())
 
 	voucher1 := &models.Voucher{
 		Code:      "VOUCHER1",
@@ -336,25 +256,26 @@ func TestListVouchersHandler(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Run("Test List Vouchers successfully", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
 		req, _ := http.NewRequest("GET", "/api/v1/vouchers", nil)
 		req.Header.Set("Authorization", "Bearer "+token)
 		resp := httptest.NewRecorder()
 		router.ServeHTTP(resp, req)
 		assert.Equal(t, http.StatusOK, resp.Code)
 
-		type VouchersResponse struct {
-			Message string `json:"message"`
-			Data    struct {
-				Vouchers []models.Voucher `json:"vouchers"`
-			} `json:"data"`
-		}
-
-		var vouchersResp VouchersResponse
+		var vouchersResp map[string]interface{}
 		err := json.Unmarshal(resp.Body.Bytes(), &vouchersResp)
 		assert.NoError(t, err)
-		assert.Equal(t, "Vouchers are Retrieved successfully", vouchersResp.Message)
-		vouchers := vouchersResp.Data.Vouchers
+		assert.Equal(t, "Vouchers are Retrieved successfully", vouchersResp["message"])
+		data, ok := vouchersResp["data"].(map[string]interface{})
+		assert.True(t, ok)
+		vouchersRaw, ok := data["vouchers"]
+		assert.True(t, ok)
+		vouchersBytes, err := json.Marshal(vouchersRaw)
+		assert.NoError(t, err)
+		var vouchers []models.Voucher
+		err = json.Unmarshal(vouchersBytes, &vouchers)
+		assert.NoError(t, err)
 		var found1, found2 bool
 		for _, v := range vouchers {
 			if v.Code == voucher1.Code {
@@ -390,34 +311,14 @@ func TestCreditUserHandler(t *testing.T) {
 	require.NoError(t, err)
 	router := app.router
 
-	adminUser := &models.User{
-		ID:       1,
-		Username: "Admin User",
-		Email:    "admin@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    true,
-		Mnemonic: app.config.SystemAccount.Mnemonic,
-	}
-	normalUser := &models.User{
-		ID:       2,
-		Username: "Normal User",
-		Email:    "user@example.com",
-		Password: []byte("securepassword"),
-		Verified: true,
-		Admin:    false,
-		Mnemonic: app.config.SystemAccount.Mnemonic,
-	}
-	err = app.handlers.db.RegisterUser(adminUser)
-	require.NoError(t, err)
-	err = app.handlers.db.RegisterUser(normalUser)
-	require.NoError(t, err)
+	adminUser := CreateTestUser(t, app, "admin@example.com", "Admin User", []byte("securepassword"), true, true, 0, time.Now())
+	normalUser := CreateTestUser(t, app, "user@example.com", "Normal User", []byte("securepassword"), true, false, 0, time.Now())
 
 	t.Run("Test Credit user successfully", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
-		payload := map[string]interface{}{
-			"amount": 100,
-			"memo":   "Manual credit",
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
+		payload := CreditRequestInput{
+			Amount: 100,
+			Memo:   "Manual credit",
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/users/2/credit", bytes.NewReader(body))
@@ -439,7 +340,7 @@ func TestCreditUserHandler(t *testing.T) {
 	})
 
 	t.Run("Test Credit user with invalid request format", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
 		body, _ := json.Marshal(map[string]interface{}{}) // missing required fields
 		req, _ := http.NewRequest("POST", "/api/v1/users/2/credit", bytes.NewReader(body))
 		req.Header.Set("Authorization", "Bearer "+token)
@@ -450,10 +351,10 @@ func TestCreditUserHandler(t *testing.T) {
 	})
 
 	t.Run("Test Credit user with invalid user id", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
-		payload := map[string]interface{}{
-			"amount": 100,
-			"memo":   "Manual credit",
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
+		payload := CreditRequestInput{
+			Amount: 100,
+			Memo:   "Manual credit",
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/users/abc/credit", bytes.NewReader(body))
@@ -465,10 +366,10 @@ func TestCreditUserHandler(t *testing.T) {
 	})
 
 	t.Run("Test Credit non-existing user", func(t *testing.T) {
-		token := getAdminToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username)
-		payload := map[string]interface{}{
-			"amount": 100,
-			"memo":   "Manual credit",
+		token := GetAuthToken(t, app, adminUser.ID, adminUser.Email, adminUser.Username, true)
+		payload := CreditRequestInput{
+			Amount: 100,
+			Memo:   "Manual credit",
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/users/9999/credit", bytes.NewReader(body))
@@ -480,9 +381,9 @@ func TestCreditUserHandler(t *testing.T) {
 	})
 
 	t.Run("Test Credit user with no token", func(t *testing.T) {
-		payload := map[string]interface{}{
-			"amount": 100,
-			"memo":   "Manual credit",
+		payload := CreditRequestInput{
+			Amount: 100,
+			Memo:   "Manual credit",
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/users/2/credit", bytes.NewReader(body))
@@ -494,9 +395,9 @@ func TestCreditUserHandler(t *testing.T) {
 
 	t.Run("Test Credit user with non-admin user", func(t *testing.T) {
 		token := GetAuthToken(t, app, normalUser.ID, normalUser.Email, normalUser.Username, false)
-		payload := map[string]interface{}{
-			"amount": 100,
-			"memo":   "Manual credit",
+		payload := CreditRequestInput{
+			Amount: 100,
+			Memo:   "Manual credit",
 		}
 		body, _ := json.Marshal(payload)
 		req, _ := http.NewRequest("POST", "/api/v1/users/2/credit", bytes.NewReader(body))
