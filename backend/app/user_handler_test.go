d55e043ed1d79177ab62f0cdf9ce40677dfe7159
Alaa Elattar <alaamahmoud.1223@gmail.com>
2025-08-04 16:31:13 +0300

    fix lint

--- backend/app/user_handler_test.go
+++ backend/app/user_handler_test.go
@@ -864,7 +864,7 @@ func TestAddSSHKeyHandler(t *testing.T) {
 		require.NoError(t, err)
 		router := app.router
 		user := CreateTestUser(t, app, "addsshuser@example.com", "Add SSH User", []byte("securepassword"), true, false, false, 0, time.Now())
-		
+
 		token := GetAuthToken(t, app, user.ID, user.Email, user.Username, false)
 		payload := SSHKeyInput{
 			Name:      "mykey",
@@ -889,7 +889,7 @@ func TestAddSSHKeyHandler(t *testing.T) {
 		require.NoError(t, err)
 		router := app.router
 		user := CreateTestUser(t, app, "addsshuser2@example.com", "Add SSH User2", []byte("securepassword"), true, false, false, 0, time.Now())
-		
+
 		token := GetAuthToken(t, app, user.ID, user.Email, user.Username, false)
 		// Missing public_key
 		payload := SSHKeyInput{
@@ -909,7 +909,7 @@ func TestAddSSHKeyHandler(t *testing.T) {
 		require.NoError(t, err)
 		router := app.router
 		user := CreateTestUser(t, app, "addsshuser3@example.com", "Add SSH User3", []byte("securepassword"), true, false, false, 0, time.Now())
-		
+
 		token := GetAuthToken(t, app, user.ID, user.Email, user.Username, false)
 		payload := SSHKeyInput{
 			Name:      "badkey",
@@ -933,10 +933,10 @@ func TestAddSSHKeyHandler(t *testing.T) {
 		require.NoError(t, err)
 		router := app.router
 		user := CreateTestUser(t, app, "addsshuser4@example.com", "Add SSH User4", []byte("securepassword"), true, false, false, 0, time.Now())
-		
+
 		token := GetAuthToken(t, app, user.ID, user.Email, user.Username, false)
 		publicKey := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDzy9yGz+CsKhjYB3FLr27SaoPQVi/tOZDZ06LnO7NuVUj0yR3e7IJO26cxs6j7tRAGTrA7choRMlQJdCFQfkDCaAL+31fPSihHhB3kxUTnZymaWgZ6s/JxjI/2/kKcLjxMWpMYTs18ZdRJf1DgoiyTV6yhlxAhWJvMxTtC5++h5+Ir7mHoN5QdrRt5AjKEcTEJjoKC3it4itHz7w45hi4y07kFYIk4HcMGrInh1IC/BriU7xKlwYcP2tp0W4GIraDJoD8OR3cgcYd/AFXSnVDtomCq5MaKBUli6FWLCK7E3+0AtYxxLkQ/zFkPsYSFAGGqVp8uq2hI46d0TxhgcG2EsWiF/2yOjtMdX1ab3Ns23p8Q0l/8JxXn6WT9xhme9eb2v8UjukN0AR8j+hp5xoQuSEgXAxkg4PFEa2seYEcE8xZPOSavuQl4wEAjXH/1BHnRHxrBBWixN2xdclHRAKQRwR+EHg8wDQ0EAAxtoCCAVHOepBrmV0JDxJGHQ8euvbs= test@gmail.com"
-		
+
 		// Add first SSH key
 		payload1 := SSHKeyInput{
 			Name:      "keyA",
@@ -949,7 +949,7 @@ func TestAddSSHKeyHandler(t *testing.T) {
 		resp1 := httptest.NewRecorder()
 		router.ServeHTTP(resp1, req1)
 		assert.Equal(t, http.StatusCreated, resp1.Code)
-		
+
 		payload2 := SSHKeyInput{
 			Name:      "keyB",
 			PublicKey: publicKey,
